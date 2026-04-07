8c4ebab02fc139993573b9c8296f14e84a509f9c
Ayomide <ayomidejai0@gmail.com>
2026-04-07 14:10:53 +0100

    Fixed uncaught merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,27 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AcademicHome from "./pages/Academic/AcademicHome";
 import StudyMaterials from "./pages/Academic/StudyMaterials";
 import StudyGroups from "./pages/Academic/StudyGroups";
 import ExamSchedule from "./pages/Academic/ExamSchedule";
 import Resources from "./pages/Academic/Resources";
-import { Routes, Route } from "react-router-dom";
-import Login from './pages/Login/Login.jsx'
+import Login from "./pages/Login/Login.jsx";
 import Onboard from "./pages/Onboarding/Onboard.jsx";
-import MarketHome from './pages/Marketplace/MarketHome.jsx';
+import MarketHome from "./pages/Marketplace/MarketHome.jsx";
 function App() {
-
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="/Onboard" element={<Onboard />} />
-        <Route path="/marketplace" element={<MarketHome />} />
-        <Route path="/academy" element={<AcademicHome />}>
-          <Route index element={<StudyMaterials />} />
-          <Route path="study-materials" element={<StudyMaterials />} />
-          <Route path="study-groups" element={<StudyGroups />} />
-          <Route path="exam-schedule" element={<ExamSchedule />} />
-          <Route path="resources" element={<Resources />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/Onboard" element={<Onboard />} />
+      <Route path="/marketplace" element={<MarketHome />} />
+      <Route path="/academy" element={<AcademicHome />}>
+        <Route index element={<StudyMaterials />} />
+        <Route path="study-materials" element={<StudyMaterials />} />
+        <Route path="study-groups" element={<StudyGroups />} />
+        <Route path="exam-schedule" element={<ExamSchedule />} />
+        <Route path="resources" element={<Resources />} />
+      </Route>
+    </Routes>
   );
 }
 
