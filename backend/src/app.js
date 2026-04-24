06694c178ca9bfaf1914fe4900c1f22eec0ffcbf
Joshua Omotoso <jae5omotoso@gmail.com>
2026-04-24 08:04:34 +0100

    Fixed slash mistake(im a dumbass)

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const path = require("path");
 
 const app = express();
 app.use(cors({
-    origin: "https://student-hub-henna-nu.vercel.app/",
+    origin: "https://student-hub-henna-nu.vercel.app",
     credentials: true,
 }));
 app.use(express.json());
