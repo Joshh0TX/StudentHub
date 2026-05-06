31ba8ccad21f688cf1ee2bbf82a242d0cc8a0357
Joshua Omotoso <jae5omotoso@gmail.com>
2026-05-06 16:14:13 +0100

    Fixed backend image upload issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,6 +18,8 @@ app.use(cors({
         "http://localhost:3000",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
 app.use('/api/groups', groupRoutes);
