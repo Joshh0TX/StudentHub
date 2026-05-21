7268b108ad74c51139ec74bf711830ffc0d63b89
nifemi <nife.osanyin@gmail.com>
2026-05-21 14:59:55 +0100

    fixed abisola's error :)

--- frontend/src/pages/Marketplace/MyOrders.jsx
+++ frontend/src/pages/Marketplace/MyOrders.jsx
@@ -1,7 +1,7 @@
 import "./MyOrders.css";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { fetchBuyerOrders } from "./marketplaceApi";
+// import { fetchBuyerOrders } from "./marketplaceApi";
 import { getUser } from "./testUser";
 import API_BASE from "../../config";
 
